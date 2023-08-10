51a9b58948fe23f024181f61bd451ba36b9fcf94
shubhamg123 <59757880+shubhamg123@users.noreply.github.com>
2023-08-10 20:36:10 +0530

    Update rainbow.js

--- rainbow.js
+++ rainbow.js
@@ -1,121 +1,100 @@
-var container=document.getElementById('container')
-var svg=document.getElementById('svg')
-var season=document.getElementById('seasons')
-var daytime=document.getElementById('daytime')
-var time=document.getElementById('time')
-function builderseason(e){
-  console.log(e.value)
-  if(e.value==='winter')
-  { container.innerHTML=''
-   const x=`<svg width="800" height="400" id="svg" >
-       
-   <circle cx="400" cy="450" r="200" stroke="none"
-       stroke-width="2" fill="blue" />
-
-   <!-- r defines the radius of the circle  -->
-   <circle cx="400" cy="450" r="180" stroke="none" 
-       stroke-width="2" fill="#ADD8E6" />
-   <circle cx="400" cy="450" r="175" stroke="none" 
-       stroke-width="2" fill="#007FFF" />
-   <circle cx="400" cy="450" r="150" stroke="none" 
-       stroke-width="2" fill="#4166F5" />
-   <circle cx="400" cy="450" r="125" stroke="none" 
-       stroke-width="2" fill="#0067A5" />
-   <circle cx="400" cy="450" r="100" stroke="none" 
-       stroke-width="2" fill="#OF4D92" />
-   <circle cx="400" cy="450" r="90" stroke="none" 
-       stroke-width="2" fill="darkblue" />
-</svg>`
-container.innerHTML=x;
+window.onload = function () {
+    // Your code here
+    const container = document.getElementById('container');
+    var seasons = document.getElementById('seasons');
+    var daytime = document.getElementById('daytime');
+    var time = document.getElementById('time');
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('width', '800');
+    svg.setAttribute('height', '400');
+    svg.id = 'svg';
+    container.appendChild(svg);
 
 
+    function buildSVG(circles) {
+        svg.innerHTML = '';
+        circles.forEach(circle => svg.appendChild(circle));
+    }
+    // Define the builderseason function
+    function builderseason(e) {
+        const colors = {
+            summer: ['red', 'yellow', 'orange', 'red', 'yellow', 'orange', 'red'],
+            winter: ['blue', '#ADD8E6', '#007FFF', '#4166F5', '#0067A5', '#OF4D92', 'darkblue'],
+            spring: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+        };
+        const circleRadius = [200, 180, 175, 150, 125, 100, 90];
 
-  }
- else if(e.value==='summer')
-  { container.innerHTML=''
-   const x=`<svg width="800" height="400" id="svg" >
-       
-   <circle cx="400" cy="450" r="200" stroke="none"
-       stroke-width="2" fill="red" />
+        const colorList = e ? colors[e.value] : colors.summer;
+        const circles = colorList.map((color, index) => {
+            const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+            circle.setAttribute('cx', '400');
+            circle.setAttribute('cy', '450');
+            circle.setAttribute('r', circleRadius[index]);
+            circle.setAttribute('stroke', 'none');
+            circle.setAttribute('stroke-width', '2');
+            circle.setAttribute('fill', color);
+            return circle;
+        });
 
-   <!-- r defines the radius of the circle  -->
-   <circle cx="400" cy="450" r="180" stroke="none" 
-       stroke-width="2" fill="yellow" />
-   <circle cx="400" cy="450" r="175" stroke="none" 
-       stroke-width="2" fill="orange" />
-   <circle cx="400" cy="450" r="150" stroke="none" 
-       stroke-width="2" fill="red" />
-   <circle cx="400" cy="450" r="125" stroke="none" 
-       stroke-width="2" fill="yellow" />
-   <circle cx="400" cy="450" r="100" stroke="none" 
-       stroke-width="2" fill="orange" />
-   <circle cx="400" cy="450" r="90" stroke="none" 
-       stroke-width="2" fill="red" />
-</svg>`
-container.innerHTML=x;
+        buildSVG(circles);
+    }
 
+    // Define the builderdaytime function
+    function builderdaytime(event) {
+        const backgrounds = {
+            morning: 'violet',
+            noon: 'white',
+            dusk: 'black'
+        };
+        const selectedOption1 = event.target.options[event.target.selectedIndex];
+        const colorvalue = selectedOption1.getAttribute('value');
 
+        var color = backgrounds[colorvalue];
+        var circle = document.getElementsByTagName('circle')[0];
+        circle.removeAttribute('class');
+        const filter = document.createElementNS("http://www.w3.org/2000/svg", "filter");
+        filter.setAttribute("id", "dynamicShadow");
+        var svg = document.getElementById('svg');
+        const existingFilter = svg.querySelector("#dynamicShadow");
+        if (existingFilter) {
+            svg.removeChild(existingFilter);
+        }
+        svg.appendChild(filter);
+        const dropShadow = document.createElementNS("http://www.w3.org/2000/svg", "feDropShadow");
+        dropShadow.setAttribute("dx", 10);
+        dropShadow.setAttribute("dy", 15);
+        dropShadow.setAttribute("stdDeviation", 15);
+        dropShadow.setAttributeNS(null, "flood-color", color);
+        dropShadow.setAttribute("flood-opacity", 0.9);
+        filter.appendChild(dropShadow);
 
-  }
-  else if(e.value==='spring')
-  { container.innerHTML=''
-   const x=`<svg width="800" height="400" id="svg" >
-       
-   <circle cx="400" cy="450" r="200" stroke="none"
-       stroke-width="2" fill="red" />
-
-   <!-- r defines the radius of the circle  -->
-   <circle cx="400" cy="450" r="180" stroke="none" 
-       stroke-width="2" fill="orange" />
-   <circle cx="400" cy="450" r="175" stroke="none" 
-       stroke-width="2" fill="yellow" />
-   <circle cx="400" cy="450" r="150" stroke="none" 
-       stroke-width="2" fill="green" />
-   <circle cx="400" cy="450" r="125" stroke="none" 
-       stroke-width="2" fill="blue" />
-   <circle cx="400" cy="450" r="100" stroke="none" 
-       stroke-width="2" fill="indigo" />
-   <circle cx="400" cy="450" r="90" stroke="none" 
-       stroke-width="2" fill="violet" />
-</svg>`
-container.innerHTML=x;
-
-
-
-  }
-
-}
-function builderdaytime(e){
-      console.log(e.value)
-if(e.value=='noon')
-{   svg.removeAttribute('class')
-    svg.classList.add('normal')
-    console.log(svg.classList)
-}
-if(e.value=='morning'){
-    svg.removeAttribute('class')
-    svg.classList.add('white')
-    console.log(svg.classList)
-}
-if(e.value=='dusk')
-{     svg.removeAttribute('class')
-    svg.classList.add('dusk')
-    console.log(svg.classList)
-}
+        circle.setAttribute("filter", "url(#dynamicShadow)");
 
 
-}
-function buildertime(f){
-    const svgs=document.getElementById('svg')
-  svgs.style.transform = 'rotate('+f+'deg)';
-  console.log(svg)
-console.log(f)
+        // circle[0].classList.add(backgrounds[e.value]);
+    }
 
-}
+    // Define the buildertime function
+    function buildertime(event) {
+        const selectedOption1 = event.target.options[event.target.selectedIndex];
+        const degree = selectedOption1.getAttribute('degree');
+        console.log(degree);
+        svg.style.transform = `rotate(${degree}deg)`;
+    }
 
+    // Attach event listeners
+    seasons.addEventListener('change', function () {
+        builderseason(this);
+    });
 
+    daytime.addEventListener('change', function () {
+        builderdaytime(event);
+    });
 
+    time.addEventListener('change', function () {
+        buildertime(event);
+    });
 
+    builderseason();
 
-
-
+};
